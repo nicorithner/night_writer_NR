bfc0b1c1f62bda4f4181479f6e8e34871f512528
Nico Rithner <nicorithner@gmail.com>
2020-07-31 13:41:10 -0600

    Add test for translate method to assert it can modify the content of a given file

--- test/night_writer_test.rb
+++ test/night_writer_test.rb
@@ -4,7 +4,13 @@ class NightWriterTest < Minitest::Test
 
   def test_it_exists
     nightwriter = NightWriter.new("data/message.txt", "data/translated.txt")
+
     assert_instance_of NightWriter, nightwriter
   end
-
+  
+  def test_it_can_modify_the_input_file
+    nightwriter = NightWriter.new("data/message.txt", "data/modified_text.txt")
+    
+    assert_equal "hello modified text", nightwriter.translate("data/message.txt")
+  end
 end
