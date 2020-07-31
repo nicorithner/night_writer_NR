33435c40cdaf11a21679a8de395ef5877b59d5f3
Nico Rithner <nicorithner@gmail.com>
2020-07-31 09:10:28 -0600

    Add test text to method write file

--- lib/processable.rb
+++ lib/processable.rb
@@ -9,14 +9,14 @@ module Processable
 
   def write_file(output_file)
     file = File.open(output_file, "r")
-    
+
     if file.read.empty?
       File.open(output_file, "w") do |f|
-        f.write "#{Time.now} - User logged in\n"
+        f.write "#{Time.now} - Test text (when file is empty)\n"
       end
     else
       File.open(output_file, "a") do |f|
-        f.write "#{Time.now} - User logged in\n"
+        f.write "#{Time.now} - Test text for when file is not empty\n"
       end
     end
   end
