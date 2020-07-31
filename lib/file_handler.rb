a7bca477562f83af5698980c72dff872610bcbab
Nico Rithner <nicorithner@gmail.com>
2020-07-30 19:44:20 -0600

    Add test it can read a file

--- lib/file_handler.rb
+++ lib/file_handler.rb
@@ -1,4 +1,16 @@
 class FileHandler
+  attr_reader :file_content
+  def initialize(input_file)
+    @input_file = input_file
+    read_file
+  end
+
+  def read_file
+    filename = File.open(@input_file, "r")
+    @file_content = filename.read
+    filename.close
+    # binding.pry
+  end
 
 
 end
