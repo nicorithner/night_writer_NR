32aad585966a3fc11a5c2637a69638e51205d8b1
Nico Rithner <nicorithner@gmail.com>
2020-08-03 15:28:19 -0600

    Update tests for processable to reference to new classes

--- test/processable_test.rb
+++ test/processable_test.rb
@@ -4,21 +4,21 @@ class ProcessableTest < Minitest::Test
 
   def test_read_a_file
     # skip
-    test_object = NightWriter.new("data/message.txt", "data/translated.txt")
-    assert_equal "hello world", test_object.read_file("data/message.txt")
+    translator = EnglishTranslator.new("data/message.txt", "data/translated.txt")
+    assert_equal "hello world", translator.read_file("data/message.txt")
   end
 
   def test_it_can_read_a_file
     # skip
-    test_object = NightWriter.new("data/message.txt", "data/translated.txt")
-    assert_equal "hello world", test_object.recieve_and_read_file
+    translator = EnglishTranslator.new("data/message.txt", "data/translated.txt")
+    assert_equal "hello world", translator.recieve_and_read_file
   end
 
   def test_it_can_write_to_a_file
     # skip
-    test_object = NightWriter.new("data/message.txt", "data/processable_can_write.txt")
-    test_object.can_modify
-    assert_equal "hello modified text world", test_object.read_test_file("data/translated.txt")
+    translator = EnglishTranslator.new("data/message.txt", "data/processable_can_write.txt")
+    translator.can_modify
+    assert_equal "hello modified text world", translator.read_test_file("data/translated.txt")
   end
 
 end
